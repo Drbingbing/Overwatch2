c5c24b0eb5d1728c794763ef2f7247e7bc12c56a
Drbingbing <tw718140@gmail.com>
2023-09-13 17:24:10 +0800

    Initial Commit

---
+++ Overwatch2UITests/Overwatch2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Overwatch2UITestsLaunchTests.swift
+//  Overwatch2UITests
+//
+//  Created by 鍾秉辰 on 2023/9/13.
+//
+
+import XCTest
+
+final class Overwatch2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
